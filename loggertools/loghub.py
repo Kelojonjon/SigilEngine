5ffe4b83ee716785590cc8250268b1918f89678c
ensio <rokkasusi@gmail.com>
2025-05-12 23:03:24 +0300

    docstringed the loghub

--- loggertools/loghub.py
+++ loggertools/loghub.py
@@ -8,57 +8,67 @@ from utilities.timermodule import TIMER
 from sigilengine.space import LOGGER_SPACE
 from sigilengine.space import LOGGER_LOCK
 
+
 class LOGHUB():
+    """
+    Threaded module that hosts queues for logrecords and commands.
+    
+    Logrecords are filtered and written to a file in format:  asctime | levelname | owner | entity_id | message
+    Logs are rotated between 3 files using RotatingFileHandler.
+    Records with levelno that's equal or above 40, get handled ASAP, lower loglevels are written in batches
+    
+    Currently all loglevels are collected via 1 public queue
+    Uses buffering and timed event scheduling to limit I/O spam
+    Filters levels with True flags, and uses sample rating.
+    
+    Command queue is defined but not currently handled.
+    """
     
     def __init__(self, hub_id):
-        """
-        Missing command_queue handling currently
-        """
+        
         # Alive flag
         self.alive = True
-        # Similar to the canvases, loggers can be plenty,
-        # they register themselves to the LOGGER_SPACE to be interacted with  
+       
+        # Name of the hub 
         self.hub_id = hub_id
         
-            
-        # Where all the logs are sent to be handled
+        # Queues hosted in the LOGGER_SPACE
         self.hub_queue = Queue()
-        # Queue to send command packages to the central logger
         self.command_queue = Queue()
         
         
-        # We will save the logs inside the "logs" subfolder of "loggertools"
-        # Also handle creating a folder is none exists
+        # Log folder path, creates a "logs" folder, that is stored in the same folder that the code is being executed from.
         self.log_folder_path = Path(__file__).parent / "logs"
         self.log_folder_path.mkdir(parents=True, exist_ok=True)
         self.log_file = self.log_folder_path / f"{self.hub_id}.log"
         # The size of log files in MB
         self.log_file_size = 5
         
-        # This logger will take the records format them, and write to the file
+        # File I/O is handled via sublogger and a RotatingFileHandler
         self.minilogger = logging.getLogger(self.hub_id)
         self.rotating_handler = RotatingFileHandler(
             self.log_file,
             maxBytes=self.log_file_size * 1024 * 1024,
             backupCount=3
         )
-        
-        # Format the log records and add handler to the minilogger
-        formatter = logging.Formatter('%(asctime)s  | %(levelname)s | %(owner)s | %(entity_id)s | %(message)s')
-        
-        #TEST FORMATTER! MISSING OWNER AND CANVA_ID
-        #formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-
         self.minilogger.addHandler(self.rotating_handler)
+        
+        # Format the records, 
+        formatter = logging.Formatter('%(asctime)s  | %(levelname)s | %(owner)s | %(entity_id)s | %(message)s')
         self.rotating_handler.setFormatter(formatter)
         
-        # We will write logs that are < loglevel 40 only every second, to prevent I/O spam
-        # Errors and criticals are written instantly as they arrive, to hopefully catch them 
+        
+        # A Buffer holding records before the I/O cycle, thats handled using a timer 
         self.file_buffer = []
         
+        # Timer that flushes the buffered records to a file
+        self.timer = TIMER()
+        self.timer.event(write_to_file=1) # 1 second interval
+        self.timer.action(write_to_file=self.write_buffered_records)
         
-        # Flags to decide if we handle levels
-        # Both filters work directly with the logger levelno's
+        
+        # Filtering using True flags and Sample rating
+        # True == Records get passed trough
         self.level_filter = {
             10: True,   # DEBUG
             20: True,   # INFO
@@ -67,7 +77,7 @@ class LOGHUB():
             50: True    # CRITICAL
         }
         
-        # Sample rates for different levels to reduce spam
+        # Sample rating, default is 1/1
         self.sample_filter = {
             10: 1, # DEBUG
             20: 1, # INFO
@@ -76,7 +86,7 @@ class LOGHUB():
             50: 1  # CRITICAL
         }
 
-        # Helper counter for the sample_filtering
+        # Used in keeping track of the sample rates.
         self.sample_counter = {
             10: 0, # DEBUG
             20: 0, # INFO
@@ -85,15 +95,13 @@ class LOGHUB():
             50: 0  # CRITICAL
         }
 
-        self.timer = TIMER()
-        self.timer.event(write_to_file=1)
-        self.timer.action(write_to_file=self.write_buffered_records)
+
     ##################################################################################################
     
     
     def filter(self, record):
         """
-        Appliess the filtering rules to a single record
+        Appliess the filtering rules to a single logrecord
         """
         levelno = record.levelno
         
@@ -102,9 +110,9 @@ class LOGHUB():
         if condition == False:
             return None
         
-        # Cycle the sample counter  
         counter = self.sample_counter.get(levelno, 1) + 1
-        # If counter goes over the sample rate, reset and return the record
+        
+        # If counter goes over the sample rate, reset and return the logrecord
         if counter >= self.sample_filter.get(levelno, 1):
             self.sample_counter[levelno] = 0
             return record
@@ -114,8 +122,7 @@ class LOGHUB():
         
     def write_buffered_records(self):
         """
-        Writes the whole file_buffer to a file, and flushes
-        Timer
+        Flushes the logrecords from the filebuffer, and writes them to a file
         """
         if len(self.file_buffer) > 0:
             for record in self.file_buffer:
@@ -124,6 +131,10 @@ class LOGHUB():
             
            
     def handle_my_record(self, record):
+        """
+        Either handles important logrecords ASAP, or appends them to the I/O file_buffer
+        """
+        
         levelno = record.levelno
         
         if record == None:
@@ -132,28 +143,35 @@ class LOGHUB():
         # Handle high priority without buffer        
         if levelno >= 40:
             self.minilogger.handle(record)
-        # Append the record to file_buffer
+        # Append the logrecord to file_buffer
         if levelno < 40:
             self.file_buffer.append(record)
        
               
     def gatekeeper(self, item):
         """
-        Flattens the lists, routes the records trough the filter
-        and then sends the records to be handled
+        Entry point for all incoming records.
+        Handles the flattening, if a item is a list
+        Calls handle_my_record for all the records.
         """
+        # If item is a list, flatten it down, and pass forward
         if isinstance(item, list):
             for record in item:
                 filtered_record = self.filter(record)
                 self.handle_my_record(filtered_record)
+        # Else just pass forward
         else:
             filtered_item = self.filter(item)
             self.handle_my_record(filtered_item)
                 
             
     def run(self):
-
-        # Init 
+        """
+        Runs the loop
+        Loghub registers itself to LOGGER_SPACE
+        Loops and listens for the queue/s
+        """
+        # Registers to LOGGER_SPACE
         try:
             with LOGGER_LOCK:
                 LOGGER_SPACE[self.hub_id] = {
@@ -168,16 +186,17 @@ class LOGHUB():
         try:
             while self.alive:
                 try:
-                    # Check if time to write to the file
-                    # Executes the write_buffered_records when it is the time
+                    # Checks if  it is time to write to the file
                     self.timer.update_timer()
 
+                    # Handles incoming Traffic
                     item = self.hub_queue.get(timeout=0.1)
                     self.gatekeeper(item)
+                
                 except Exception:
                     continue # Idle loop
             
-        # Shutdown    
+        # Deletes itself from LOGGER_SPACE when the loop ends 
         finally:
             with LOGGER_LOCK:
                 LOGGER_SPACE.pop(self.hub_id, None)
